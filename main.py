4229c0a748a4cfb1f3e5ae342c09b6e7893d9024
dg2026510 <dg2026510@danggok.hs.kr>
2026-05-29 11:26:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ group1 = group()
 import random
 box(size = vec(10,15,10),pos = vec(0,0,0),color = color.red,group=group1)
 box(size = vec(10,5,5),pos = vec(0,-5,7.5),color = color.red,group=group1)
-cylinder(size = vec(10,15,10),pos = vec(-5,6,0),color = color.red,group=group1)
+cylinder(size = vec(10,15,10),pos = vec(-5,7,0),color = color.red,group=group1)
 cylinder(size = vec(0.5,5,5),pos = vec(5,1.5,2.5),color = color.yellow)
 cylinder(size = vec(0.5,5,5),pos = vec(5.75,1.5,2.5),color = color.yellow)
 cylinder(size = vec(0.4,4.5,4.5),pos = vec(5.5,1.5,2.5),color = color.yellow)
@@ -14,7 +14,7 @@ box(size = vec(5.5,1,2.5),pos = vec(0,-2.75,8),color = color.black)
 box(size = vec(11,2,17),pos = vec(0,-7,2),color = color.black)
 box(size = vec(35,0.25,35),pos = vec(0,-7,5),texture = textures.wood)
 #model
-tx = text(text='press spacebar yay', align='center', color=color.green, pos = vec(0,15,0))
+tx = text(text='press spacebar yay', align='center', color=color.green, pos = vec(0,16,0))
 #text
 a = box(size = vec(2.25,2.25,2.25),pos = vec(0,3,4.25),texture = 'https://st.depositphotos.com/1006187/4695/i/450/depositphotos_46951539-stock-photo-number-from-gold.jpg')
 b = box(size = vec(2.25,2.25,2.25),pos = vec(3,3,4.25),texture = 'https://st.depositphotos.com/1006187/4695/i/450/depositphotos_46951539-stock-photo-number-from-gold.jpg')
